d2389b64e33b892294ab355d67548e0522964c96
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2025-03-26 20:12:18 +0100

    feat(config): ✨ add Cloudflare adapter and update worker configuration
    Introduce Cloudflare adapter in Astro config and add worker configuration files.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from "./src/locales";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://astro-cms-dpv.pages.dev", // Set your site's URL
+	adapter: cloudflare({
+		platformProxy: {
+		  enabled: true
+		}
+	  }),
 	i18n: {
 		defaultLocale: DEFAULT_LOCALE_SETTING,
 		locales: Object.keys(LOCALES_SETTING),
